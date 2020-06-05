1cb1c1e667253a0d839850fe741ada1b90eabcf1
Renata <safaryonok@gmail.com>
2020-06-05 22:47:23 +0300

    Classwork
    Classwork

---
+++ cmake-build-debug/CMakeFiles/26_11_2019_1_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "26_11_2019_1_.exe"
+  "26_11_2019_1_.exe.manifest"
+  "26_11_2019_1_.pdb"
+  "CMakeFiles/26_11_2019_1_.dir/main.cpp.obj"
+  "lib26_11_2019_1_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/26_11_2019_1_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a;
+   cout<<"Input a";
+   cin>>a;
+   int i=0;
+   while (i<a)
+   {
+       cout<<i<<endl;
+       i++;
+   }
+}
